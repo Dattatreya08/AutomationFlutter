e922cafcf887a86fe37dcd3bc3b89d6ab259231d
Dattatreya <nerelladattatreya29568@gmail.com>
2023-07-03 18:10:50 +0530

    Bluetooth Paired Avaliable Devices List

--- lib/screens/bluetooth_connectivity.dart
+++ lib/screens/bluetooth_connectivity.dart
@@ -7,13 +7,34 @@ class BluetoothDevicesPage extends StatefulWidget {
 }
 
 class _BluetoothDevicesPageState extends State<BluetoothDevicesPage> {
-  List<BluetoothDevice> _devicesList = [];
+  List<BluetoothDevice> _pairedDevices = [];
+  List<BluetoothDevice> _availableDevices = [];
   bool _isDiscovering = false;
+  bool? _isBluetoothEnabled;
 
   @override
   void initState() {
     super.initState();
+    _checkBluetoothStatus();
     _startDiscovery();
+    _getPairedDevices();
+  }
+
+  Future<void> _checkBluetoothStatus() async {
+    bool? isEnabled = await FlutterBluetoothSerial.instance.isEnabled;
+    setState(() {
+      _isBluetoothEnabled = isEnabled;
+    });
+  }
+
+  Future<void> _enableBluetooth() async {
+    await FlutterBluetoothSerial.instance.requestEnable();
+    _checkBluetoothStatus();
+  }
+
+  Future<void> _disableBluetooth() async {
+    await FlutterBluetoothSerial.instance.requestDisable();
+    _checkBluetoothStatus();
   }
 
   @override
@@ -22,14 +43,14 @@ class _BluetoothDevicesPageState extends State<BluetoothDevicesPage> {
     super.dispose();
   }
 
-  void _startDiscovery() async {
+  dynamic _startDiscovery() async {
     setState(() {
       _isDiscovering = true;
     });
 
     FlutterBluetoothSerial.instance.startDiscovery().listen((r) {
       setState(() {
-        _devicesList.add(r.device);
+        _availableDevices.add(r.device);
       });
     }).onDone(() {
       setState(() {
@@ -42,31 +63,94 @@ class _BluetoothDevicesPageState extends State<BluetoothDevicesPage> {
     FlutterBluetoothSerial.instance.cancelDiscovery();
   }
 
+  Future<void> _getPairedDevices() async {
+    List<BluetoothDevice> devices =
+    await FlutterBluetoothSerial.instance.getBondedDevices();
+    setState(() {
+      _pairedDevices = devices;
+    });
+  }
+
+  Future<void> _refreshDevices() async {
+    setState(() {
+      _availableDevices.clear();
+    });
+    await _startDiscovery();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if (_isDiscovering)
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircularProgressIndicator(),
-          ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: _devicesList.length,
-            itemBuilder: (context, index) {
-              BluetoothDevice device = _devicesList[index];
-              return ListTile(
-                title: Text(device.address ?? ''),
-                subtitle: Text(device.address),
-                onTap: () {
-                  // Handle device selection
+    return RefreshIndicator(
+      onRefresh: () => _refreshDevices(),
+      child: ListView.builder(
+        itemCount: _pairedDevices.length + _availableDevices.length + 3,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            // Display toggle button for Bluetooth
+            return ListTile(
+              title: Text('Bluetooth'),
+              trailing: Switch(
+                value: _isBluetoothEnabled ?? false,
+                onChanged: (value) {
+                  if (value) {
+                    _enableBluetooth();
+                  } else {
+                    _disableBluetooth();
+                  }
                 },
-              );
-            },
-          ),
-        ),
-      ],
+              ),
+            );
+          }   else if (index == 1) {
+            // Display "Paired Devices" ListTile
+            return ListTile(
+                            title: Text('Paired Devices'),
+              dense: true,
+            );
+          } else if (index <= _pairedDevices.length + 1) {
+            // Display paired device ListTile
+            BluetoothDevice device = _pairedDevices[index - 2];
+            return ListTile(
+              leading: Icon(Icons.devices_other_outlined),
+              title: Text(device.name ?? 'Unknown Device'),
+              subtitle: Text(device.address),
+              onTap: () {
+                // Handle device selection
+              },
+            );
+          } else if (index == _pairedDevices.length + 2) {
+            // Display "Available Devices" ListTile with refresh button
+            return ListTile(
+              title: const Text('Available Devices'),
+              dense: true,
+              trailing: _isDiscovering
+                  ? const SizedBox(
+                width: 20,
+                height: 20,
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                ),
+              )
+                  : TextButton(
+                onPressed: _refreshDevices,
+                child: const Text('Refresh'),
+              ),
+            );
+          } else {
+            // Display available device ListTile
+            BluetoothDevice device =
+            _availableDevices[index - _pairedDevices.length - 3];
+            return ListTile(
+              leading: const Icon(Icons.device_hub_outlined),
+              title: Text(device.name ?? 'Unknown Device'),
+              subtitle: Text(device.address),
+              onTap: () {
+                // Handle device selection
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
